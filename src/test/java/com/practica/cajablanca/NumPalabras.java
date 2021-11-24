c2bc0e3fc44a1d78233663de6696d1e2ab0ff623
carlos-delpozo-gomez <carlos.delpozo.gomez@alumnos.upm.es>
2021-11-24 19:02:13 +0100

    NumPalabras acabado

--- src/test/java/com/practica/cajablanca/NumPalabras.java
+++ src/test/java/com/practica/cajablanca/NumPalabras.java
@@ -23,36 +23,43 @@ public class NumPalabras {
     @Test
     public void PathTwo(){
         Editor editor = new Editor();
-        assertThrows(java.lang.IllegalArgumentException.class, () -> {editor.numPalabras(1, -4, "hola");});
+        assertThrows(java.lang.IllegalArgumentException.class, () -> {editor.numPalabras(1, 4000, "hola");});
 
     }
 
     @Test
     public void PathThree(){
         Editor editor = new Editor();
-        assertEquals(0, editor.numPalabras(0, 1, "hola"));
+        assertEquals(0, editor.numPalabras(1, 0, "hola"));
     }
 
     @Test
     public void PathFourth(){
         Editor editor = new Editor();
+        editor.leerFichero("FicheroParaLeer.txt");
         assertEquals(0, editor.numPalabras(2, 1, "hola"));
-
     }
 
     @Test
     public void PathFive(){
         Editor editor = new Editor();
         editor.leerFichero("FicheroParaLeer.txt");
-        assertEquals(0, editor.numPalabras(1, 10, "hola"));
-
+        assertEquals(0, editor.numPalabras(1, 5, "hola"));
     }
 
     @Test
     public void PathSix(){
         Editor editor = new Editor();
         editor.leerFichero("FicheroParaLeer.txt");
-        assertEquals(2, editor.numPalabras(1, 30, "día"));
+        assertEquals(3, editor.numPalabras(1, 5, "días"));
+    }
 
+    @Test
+    public void PathSeven() {
+        Editor editor = new Editor();
+        editor.leerFichero("FicheroLeerVacioConVariosSaltosDeLinea.txt");
+        assertEquals(0, editor.numPalabras(1, 5, "día"));
     }
 }
+
+
